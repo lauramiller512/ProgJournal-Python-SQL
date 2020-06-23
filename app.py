89356d91b42a4efb156b63b6fc05a63ba9a55c31
Laura Miller <lauramiller.spence@gmail.com>
2020-06-23 11:51:28 +0100

    Created functions for adding and viewing journal entries

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from database import add_entry, view_entries
+
 menu = """Please select one of the following options:
 1) Add new entry for today.
 2) View entries.
@@ -7,13 +9,17 @@ Your selection: """
 
 welcome = "Welcome to the programming diary!"
 
+
+
 print(welcome)
 
-while (user_input := input(menu)) != "3":
-    if user_input = "1":
-        print("Adding...")
-    elif user_input = "2":
-        print("Viewing...")
+user_input = input(menu)
+
+while user_input != "3":
+    if user_input == "1":
+        add_entry()
+    elif user_input == "2":
+        view_entries()
     else:
         print("Invalid option, please try again!")
 
