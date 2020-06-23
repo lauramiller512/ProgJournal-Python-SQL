44f4f05a0b3e48a8a6ea74bba4305a1c1aafc96f
Laura Miller <lauramiller.spence@gmail.com>
2020-06-23 11:35:38 +0100

    Adding the skeleton for the app

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-menu = ""Please select one of the following options:
+menu = """Please select one of the following options:
 1) Add new entry for today.
 2) View entries.
 3) Exit.
 
-Your selection: ""
+Your selection: """
 
 welcome = "Welcome to the programming diary!"
 
-user_input = input(menu)
+print(welcome)
+
+while (user_input := input(menu)) != "3":
+    if user_input = "1":
+        print("Adding...")
+    elif user_input = "2":
+        print("Viewing...")
+    else:
+        print("Invalid option, please try again!")
+
