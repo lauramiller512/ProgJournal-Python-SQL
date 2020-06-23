1acbd13c60594a164bebb506099875bd3a9ea50b
Laura Miller <lauramiller.spence@gmail.com>
2020-06-23 11:17:33 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+menu = ""Please select one of the following options:
+1) Add new entry for today.
+2) View entries.
+3) Exit.
+
+Your selection: ""
+
+welcome = "Welcome to the programming diary!"
+
+user_input = input(menu)
