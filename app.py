f4f375952e9fec319386c469b4ba74c6eee6c7e9
Laura Miller <lauramiller.spence@gmail.com>
2020-06-23 12:54:54 +0100

    Implemented walrus operator

--- app.py
+++ app.py
@@ -9,17 +9,12 @@ Your selection: """
 
 welcome = "Welcome to the programming diary!"
 
-
-
 print(welcome)
 
-user_input = input(menu)
-
-while user_input != "3":
+while (user_input := input(menu)) != "3":
     if user_input == "1":
         add_entry()
     elif user_input == "2":
         view_entries()
     else:
         print("Invalid option, please try again!")
-
