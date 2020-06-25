3b4e245eaeb20ea514a83be7ed14c5494446b63d
Laura Miller <lauramiller.spence@gmail.com>
2020-06-25 13:16:20 +0100

    new function for create table; hardcoded a test for inserting data into new entry

--- database.py
+++ database.py
@@ -1,9 +1,15 @@
-imoprt sqlite3
+import sqlite3
+
 connection = sqlite3.connect("JournalDB.db")
+
+def create_table():
+    with connection:
+        connection.execute("CREATE TABLE entries (content TEXT, date TEXT);")
 
 
 def add_entry(entry_content, entry_date):
-    entries.append({"content": entry_content, "date": entry_date})
+    with connection:
+        connection.execute("INSERT INTO entries VALUES('This is some test content.', '01-01-2020');")
 
 def get_entries():
     return entries
